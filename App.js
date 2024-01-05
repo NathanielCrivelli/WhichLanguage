30ba40a797fa760d27fb88ca890d23858010e74e
Nathaniel Crivelli <nathaniel.crivelli@mrjaffesclass.com>
2024-01-05 14:24:21 -0800

    need to do some logic

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
 const frequency = JSON.parse(fs.readFileSync("./letterFrequency.json", "utf-8"));
+const sample = fs.readFileSync("./text.txt", "utf-8");
+let ranking = new Map();
 
 // const chisquare = function() {
 //     let chiNum = 0;
@@ -8,4 +10,21 @@ const frequency = JSON.parse(fs.readFileSync("./letterFrequency.json", "utf-8"))
 //     }
 // }
 
-console.log(frequency[0]["a"]);
+
+
+const textFreqency = function(text) {
+    const len = text.length;
+    for (let i = 0; i < len; i++) {
+        if (text.charAt(i) in ranking) {
+            ranking[text.charAt(i)]++
+        } else {
+            ranking[text.charAt(i)] = 1;
+        }
+    }
+}
+
+const removeCapsAndWhiteSpace = function(text) {
+    
+}
+
+console.log(frequency[15]);
